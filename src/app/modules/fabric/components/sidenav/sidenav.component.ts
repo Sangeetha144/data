f99b0fbb426371ae25d2fdab95bee61806d322a3
Sangeetha144 <sangeethaselvaraj144@gmail.com>
2024-02-22 11:15:26 +0530

    assests

--- src/app/modules/fabric/components/sidenav/sidenav.component.ts
+++ src/app/modules/fabric/components/sidenav/sidenav.component.ts
@@ -1,52 +1,77 @@
-import { Component } from '@angular/core';
-import { SidenavService } from '../../services/sidenav.service';
+// import { Component } from '@angular/core';
+// import { SidenavService } from '../../services/sidenav.service';
 
+// @Component({
+//   selector: 'app-sidenav',
+//   templateUrl: './sidenav.component.html',
+//   styleUrls: ['./sidenav.component.scss']
+// })
+// export class SidenavComponent {
+
+//   dataShareDropdownOpen = false;
+//   dataShareProjects: any[] = [];
+//   dbname: any[] = [];
+//   showAlternateData = false;
+ 
+//   constructor(private service: SidenavService) {}
+ 
+//   ngOnInit() {
+//     this.fetchDataShareProjects();
+//   }
+ 
+//   toggleDataShareDropdown() {
+//     this.dataShareDropdownOpen = !this.dataShareDropdownOpen;
+//   }
+ 
+//   toggleAlternateData() {
+//     this.showAlternateData = !this.showAlternateData;
+//     if (this.showAlternateData) {
+//       this.fetchDbName();
+//     }
+//   }
+ 
+ 
+//   fetchDbName() {
+//     this.service.getDbName().subscribe((data) => {
+//       this.dbname = data;
+//       this.showAlternateData = true;
+//     });
+//   }
+ 
+//   fetchDataShareProjects() {
+//     this.service.getDataShareProjects().subscribe(
+//       (data) => {
+//         this.dataShareProjects = data;
+//       },
+//       (error) => {
+//         console.error('Error fetching data:', error);
+//       }
+//     );
+//   }
+// }
+   
+
+import { Component } from '@angular/core';
+ 
 @Component({
   selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+    templateUrl: './sidenav.component.html',
+    styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent {
-
-  dataShareDropdownOpen = false;
-  dataShareProjects: any[] = [];
-  dbname: any[] = [];
-  showAlternateData = false;
+  dataShareMenuOpen: boolean = false;
+  partnerDropdownOpen: string | null = null;
+  tablesAndViewsDropdownOpen: string | null = null;
  
-  constructor(private service: SidenavService) {}
- 
-  ngOnInit() {
-    this.fetchDataShareProjects();
+  toggleDataShareMenu() {
+    this.dataShareMenuOpen = !this.dataShareMenuOpen;
   }
  
-  toggleDataShareDropdown() {
-    this.dataShareDropdownOpen = !this.dataShareDropdownOpen;
+  togglePartnerDropdown(partner: string) {
+    this.partnerDropdownOpen = this.partnerDropdownOpen === partner ? null : partner;
   }
  
-  toggleAlternateData() {
-    this.showAlternateData = !this.showAlternateData;
-    if (this.showAlternateData) {
-      this.fetchDbName();
-    }
-  }
- 
- 
-  fetchDbName() {
-    this.service.getDbName().subscribe((data) => {
-      this.dbname = data;
-      this.showAlternateData = true;
-    });
-  }
- 
-  fetchDataShareProjects() {
-    this.service.getDataShareProjects().subscribe(
-      (data) => {
-        this.dataShareProjects = data;
-      },
-      (error) => {
-        console.error('Error fetching data:', error);
-      }
-    );
+  toggleTablesAndViewsDropdown(partner: string) {
+    this.tablesAndViewsDropdownOpen = this.tablesAndViewsDropdownOpen === partner ? null : partner;
   }
 }
-   
